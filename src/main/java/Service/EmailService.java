b7ec1b4ca5ec06038056f9a5bf4cbc35a4a2ebc6
Valentina <avgonzalez87@ucatolica.edu.co>
2023-11-30 00:08:52 -0500

    se implementa controller y services

---
+++ src/main/java/Service/EmailService.java
@@ -0,0 +1,9 @@
+package Service;
+
+import model.Orden;
+
+public class EmailService {
+    public void enviarEmailConfirmacion(Orden orden) {
+        // Implementar la lógica para enviar emails de confirmación
+    }
+}
---
+++ src/main/java/model/Credenciales.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Credenciales {
+    private String correoElectronico;
+    private String contraseña;
+
+    // Getters y setters
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
